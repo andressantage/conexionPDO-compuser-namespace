39e0509459573e945a636466c3b168285b17d9d8
Cesar Andres Santana <126884365+andressantage@users.noreply.github.com>
2023-06-23 18:07:11 -0500

    Add files via upload

---
+++ PHPcomposer-crud-basic/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'apolt01-033/ph-pcomposer-crud-basic',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd173306ec420a2c7ef8d809ac5c73d9fd4d05e95',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'apolt01-033/ph-pcomposer-crud-basic' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd173306ec420a2c7ef8d809ac5c73d9fd4d05e95',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bramus/router' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'reference' => '55657b76da8a0a509250fb55b9dd24e1aa237eba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bramus/router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
